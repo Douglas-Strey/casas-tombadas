20a62a4b394d2bbedc84551614ce06671fcd9bbf
Douglas Strey <douglas.morini@hotmail.com>
2021-09-01 11:23:24 -0300

    Finalizado a tela de login e cadastro de usuario

--- src/action/createAuth.php
+++ src/action/createAuth.php
@@ -1,17 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="/src/style/global.css" />
+    <link rel="stylesheet" href="/src/style/custom.css" />
+
+
+    <script src="./src/assets/libs/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
+
+    <title>Casas Tombadas</title>
+</head>
+
 <?php
-include '../templates/headSecond.php';
+include_once './src/helper/flashMessage/flash.php';
+
+session_start();
 ?>
 
 <body>
     <?php
-    include '../templates/navBar.php';
+    include '../template/navbar.phtml';
     ?>
 
     <div class="wrapper fadeInDown">
         <div id="formContent">
 
             <div class="fadeIn first">
-                <img src="../assets/img/icon-website.png" id="icon" alt="User Icon" />
+                <img src="#" id="icon" alt="Logo" />
                 <span>Cadastro de usuário</span>
             </div>
 
@@ -24,7 +46,7 @@ include '../templates/headSecond.php';
             </form>
 
             <div id="formFooter">
-                <a class="underlineHover" href="../action/auth.php">Já tem um login? entre aqui</a>
+                <a class="underlineHover" href="/index.php">Já tem um login? entre aqui</a>
             </div>
 
         </div>
@@ -35,6 +57,5 @@ include '../templates/headSecond.php';
     <script src="../js/auth.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
-<?php
-include '../templates/footer.php';
-?>
+
+</html>
