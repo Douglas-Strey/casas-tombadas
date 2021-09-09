8ae3034983e8fe6eca9c70c15f915b952e031e14
Douglas Strey <douglas.morini@hotmail.com>
2021-09-08 23:28:48 -0300

    Corrigida a conexão com o banco de dados, amém

--- auth/validateLogin.php
+++ auth/validateLogin.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-include_once ("../database/conection.php");
-include_once ("../hooks/functions.php");
+include '../database/conexao.php';
+include_once '../hooks/functions.php';
 
 $btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
 
