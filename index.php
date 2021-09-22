66894807b51a649804488eba19293b7d83c009c4
Douglas Strey <douglas.morini@hotmail.com>
2021-09-22 11:42:47 -0300

    Adicionado novo login, falta configurar as rotas corretas a serem utilizadas

--- index.php
+++ index.php
@@ -9,8 +9,12 @@
     <link rel="shortcut icon" href="#" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="/src/style/global.css" />
-    <link rel="stylesheet" href="/src/style/custom.css" />
 
+    <link rel="stylesheet" href="/src/login/style.css" />
+    <!-- <link rel="stylesheet" href="/src/style/custom.css" /> -->
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
 
     <script src="./src/assets/libs/jquery/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
@@ -33,7 +37,7 @@ session_start();
     endif;
     ?>
     
-    <div class="wrapper fadeInDown">
+    <!-- <div class="wrapper fadeInDown">
         <div id="formContent">
 
             <div class="fadeIn first">
@@ -52,7 +56,56 @@ session_start();
             </div>
 
         </div>
-    </div>
+    </div> -->
+
+    <div class="l-form">
+            <div class="shape1"></div>
+            <div class="shape2"></div>
+
+            <div class="form">
+                <img src="/src/login/img/authentication.svg" alt="" class="form__img">
+
+                <form action="/src/auth/validateLogin.php" method="post" class="form__content">
+                    <h1 class="form__title">Welcome</h1>
+
+                    <div class="form__div form__div-one">
+                        <div class="form__icon">
+                            <i class='bx bx-user-circle'></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Username</label>
+                            <input type="text" class="form__input">
+                        </div>
+                    </div>
+
+                    <div class="form__div">
+                        <div class="form__icon">
+                            <i class='bx bx-lock' ></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Password</label>
+                            <input type="password" class="form__input">
+                        </div>
+                    </div>
+                    <a href="#" class="form__forgot">Forgot Password?</a>
+
+                    <input type="submit" class="form__button" value="Login">
+
+                    <div class="form__social">
+                        <span class="form__social-text">Our login with</span>
+
+                        
+                    </div>
+                </form>
+            </div>
+
+        </div>
+        
+        <!-- ===== MAIN JS ===== -->
+        <script src="/src/login/main.js"></script>
+
 
     <script src="./src/assets/libs/jquery/jquery.min.js"></script>
     <script src="./src/assets/libs/sweetAlert/sweetalert2.all.min.js"></script>
