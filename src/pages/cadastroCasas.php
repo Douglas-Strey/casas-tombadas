f3af9712abc52bb95f40e46223ad72aaaecb1f5e
Douglas Strey <douglas.morini@hotmail.com>
2021-09-29 11:34:29 -0300

    Adicionado logo ao site e criado a tela de cadastro de casarões

--- src/pages/cadastroCasas.php
+++ src/pages/cadastroCasas.php
@@ -10,6 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="../style/global.css" />
 
+    <link rel="stylesheet" href="../forms/registerHouse/style.css" />
+
     <!-- ===== BOX ICONS ===== -->
     <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
 
@@ -34,6 +36,73 @@ session_start();
     endif;
     ?>
 
+<div class="l-form">
+            <div class="shape1"></div>
+            <div class="shape2"></div>
+
+            <div class="form">
+                <img src="../forms/registerHouse/img/casa-svg.svg" alt="" class="form__img">
+
+                <form action="../../auth/validateRegister.php" method="post" class="form__content">
+                    <h1 class="form__title">Cadastrar Casarão</h1>
+
+                    <div class="form__div">
+                        <div class="form__icon">
+                            <i class='bx bx-home-circle'></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Nome</label>
+                            <input type="text" id="newUserName" class="form__input" name="newUserName" autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form__div">
+                        <div class="form__icon">
+                            <i class='bx bx-camera' ></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Foto</label>
+                            <input type="text" id="newUserName" class="form__input" name="newUserName" autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form__div">
+                        <div class="form__icon">
+                            <i class='bx bx-camera' ></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Endereço</label>
+                            <input type="text" id="newUserName" class="form__input" name="newUserName" autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form__div">
+                        <div class="form__icon">
+                            <i class='bx bx-text' ></i>
+                        </div>
+
+                        <div class="form__div-input">
+                            <label for="" class="form__label">Descrição</label>
+                            <textarea id="newUserName" class="form__input" name="newUserName" autocomplete="off" required></textarea>
+                            <!-- <input type="textarea" id="newUserName" class="form__input" name="newUserName" autocomplete="off" required> -->
+                        </div>
+                    </div>
+
+                    <div class="form__items">
+                        <a href="/" class="form__forgot">Visualizar Casas Cadastradas</a>
+                    </div>
+                    
+                    <input type="submit" name="btnRegister" class="form__button" value="Criar Conta">
+                </form>
+            </div>
+
+        </div>
+        
+        <!-- ===== MAIN JS ===== -->
+        <script src="../forms/registerHouse/main.js"></script>
 
     <script src="../assets/libs/jquery/jquery.min.js"></script>
     <script src="../assets/libs/sweetAlert/sweetalert2.all.min.js"></script>
