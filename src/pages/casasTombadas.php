6b0d96bc8d8187994728db8046ea7dc9a8cb8b54
Douglas Strey <douglas.morini@hotmail.com>
2021-09-30 17:26:47 -0300

    Retornado as casas cadastradas do banco de dados com visual mínimo, mas funcional

--- src/pages/casasTombadas.php
+++ src/pages/casasTombadas.php
@@ -1,3 +1,95 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo "Teste de autenticação";
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+    <!-- ===== STYLE GLOBAL ===== -->
+    <link rel="stylesheet" href="../style/global.css" />
+
+    <!-- ===== STYLE DO ALERT ===== -->
+    <link rel="stylesheet" href="../style/alertStyle.css" />
+
+    <!-- ===== STYLE DO FORM ===== -->
+    <link rel="stylesheet" href="../forms/registerHouse/style.css" />
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Cadastro de Casas Tombadas</title>
+</head>
+
+<?php
+include '../database/conexao.php';
+include_once '../helper/flashMessage/flash.php';
+include '../hooks/functions.php';
+
+session_start();
+checkLoginData();
+?>
+
+<body>
+    <?php
+    include_once '../template/navbar-index.phtml';
+
+    // if (isset($_SESSION['flash_message'])) :
+    //     echo getFlash();
+    //     destroyFlash();
+    // endif;
+    ?>
+
+    <div class="row m-0">
+        <?php
+        // Consulta ao banco
+        $sql = "SELECT * FROM casaroes";
+        $result = $mysqli->query($sql) or die(' Erro na requisição ');
+
+        if ($result->num_rows > 0) {
+            setFlash(["Consulta realizada com sucesso!", "successCustomClass"]);
+            echo getFlash();
+            while ($row = $result->fetch_assoc()) {
+        ?>
+                <div class="col-lg-4">
+                    <div class="card w-100">
+                        <div class="card-header">
+                            <h4 class="mb-0 fs-3 fw-bold"><?= $row['nome'] ?></h4>
+                        </div>
+                        <div class="card-body">
+                            <span class="fs-5 fw-normal"><?= $row['descricao'] ?></span><br />
+                            <span class="fs-5 fw-normal"><?= $row['endereco'] ?></span><br />
+                            <img class="img-fluid" src="data:image/png;base64,<?= $row['img'] ?>" alt="" style="width:300px"> <br>
+                            <span>
+                                <button class="border-0 btn btn-danger mt-2 buton-delete">
+                                    <a class="text-white" href="../database/deleteProduct.php?produto_id='<?= $row['id'] ?>'">
+                                        Apagar Casarão
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                    </a>
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+        <?php
+            }
+        } else {
+            setFlash(["Nenhum produto cadastrado", "alertCustomClass"]);
+            echo getFlash();
+        }
+        $mysqli->close();
+        destroyFlash();
+        ?>
+    </div>
+    <div class="mt-5 ms-3">
+        <a class="btn btn-primary" href="../pages/cadastroCasas.php">Voltar</a>
+    </div>
+    <script src="../assets/libs/jquery/jquery.min.js"></script>
+    <script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
