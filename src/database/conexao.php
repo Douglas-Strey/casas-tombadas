ae0422c319ff27756bd3e087d87d965e096e1e3d
Douglas Strey <douglas.morini@hotmail.com>
2021-09-30 22:36:53 -0300

    Criado a conexão com o banco de dados de produção

--- src/database/conexao.php
+++ src/database/conexao.php
@@ -1,12 +1,13 @@
 <?php
 
-$user = "root";
-$password = "";
-$database = "faculdade";
-$hostname = "localhost";
+$user = "b9b91b640af65f";
+$password = "beda72be";
+$database = "heroku_d78033e68ed6a27";
+$hostname = "us-cdbr-east-04.cleardb.com";
 
 $mysqli = new mysqli($hostname, $user, $password, $database) or die(' Erro na conexão ');
 
 if ($mysqli->connect_errno) :
     exit("Falha ao conectar ao MySQL: " . $mysqli->connect_error);
 endif;
+mysql://:/?reconnect=true
